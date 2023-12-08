8086603b1f758fb7733e221b52ce9f23b5858c53
Rami Kader <ramy.kader.rk@gmail.com>
2023-12-08 22:31:49 +0100

    first example of a post request

--- Package.swift
+++ Package.swift
@@ -1,17 +1,18 @@
 // swift-tools-version: 5.9
-
 import PackageDescription
 
 let package = Package(
     name: "hey",
     dependencies: [
       .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+      .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.9.0"),
     ],
     targets: [
         .executableTarget(
             name: "hey",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "AsyncHTTPClient", package: "async-http-client"),
             ],
             path: "Sources"
         )
--- Sources/Hey+Commands.swift
+++ Sources/Hey+Commands.swift
@@ -1,11 +1,13 @@
 import ArgumentParser
+import AsyncHTTPClient
+import NIOHTTP1
 
 struct CodeArgs: ParsableArguments {
   @Flag(name: [.long, .short], help: "Elaborate on the result and provide explicit explanation.")
   var verbose = false
 
   @Option(name: [.long, .short], help: "Helper files to prompt as context for the assistant.")
-  var context: [String]
+  var context: [String] = []
 
   @Argument(help: "The input prompt (request) for the acting agent.")
   var prompt: [String]
@@ -21,6 +23,10 @@ struct Hey: ParsableCommand {
     defaultSubcommand: Cmd.self
   )
 
+  static func createHTTPClient() -> HTTPClient {
+    return HTTPClient(eventLoopGroupProvider: .singleton)
+  }
+
   struct Cmd: ParsableCommand {
     static var configuration = CommandConfiguration(
       commandName: "cmd",
@@ -30,8 +36,34 @@ struct Hey: ParsableCommand {
     @OptionGroup var args: CodeArgs
 
     mutating func run() throws {
-      let verbose = if !self.args.verbose { "non-" } else { "" }
-      print("Cmd[\(verbose + "verbose")]: \(args.prompt.joined(separator: " "))")
+      let httpClient = Hey.createHTTPClient()
+      defer { try! httpClient.syncShutdown() }
+
+      let bodyString = "{\"key\": \"value\"}"
+      let request = try HTTPClient.Request(
+        url: "https://webhook.site/672f9dd2-070c-44a2-9886-89e971f74e2e",
+        method: .POST,
+        headers: HTTPHeaders([("User-Agent", "Hey CLI-Assistant")]),
+        body: HTTPClient.Body.data(bodyString.data(using: .utf8)!)
+      )
+
+      let responseFuture = httpClient.execute(request: request)
+      responseFuture.whenComplete { result in
+        switch result {
+        case .failure(let error):
+          print("Client seems to have failed: \(error)")
+        case .success(let response):
+          if response.status == .ok, let body = response.body {
+            print("SUCCESS -- \(String(buffer: body))")
+          } else {
+            print("Server seems to have failed iternally -- Error code: \(response.status)")
+          }
+        }
+      }
+
+      let verbose = args.verbose ? "" : "non-"
+      print("Cmd[\(verbose + "verbose")]: with prompt[\(args.prompt.joined(separator: " "))]")
+      _ = try! responseFuture.wait()
     }
   }
 
@@ -42,14 +74,17 @@ struct Hey: ParsableCommand {
     )
 
     @Option(name: [.customLong("in")], help: "The target scripting/programming language.")
-    var lang: String = "bash" // replace with hey init conf file option or with system default shell
+    var lang: String = "bash"  // replace with hey init conf file option or with system default shell
 
-    @OptionGroup var args:CodeArgs
+    @OptionGroup var args: CodeArgs
 
     mutating func run() throws {
-      let verbose = if !self.args.verbose { "non-" } else { "" }
-      let context = if self.args.context.isEmpty { "empty" } else { self.args.context.joined(separator: ", ") }
-      print("Script[\(verbose + "verbose")]: In \(self.lang) with context[\(context)] \(args.prompt.joined(separator: " "))")
+      let verbose = args.verbose ? "" : "non-"
+      let context =
+        if self.args.context.isEmpty { "empty" } else { self.args.context.joined(separator: ", ") }
+      print(
+        "Script[\(verbose + "verbose")]: In \(self.lang) with context[\(context)] \(args.prompt.joined(separator: " "))"
+      )
     }
   }
 
