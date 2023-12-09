2403c7b8004a027675724d592e3671ee6bb3d275
Rami Kader <ramy.kader.rk@gmail.com>
2023-12-09 16:17:22 +0100

    isolate CodeArgs and Cmd

--- Sources/Hey+Commands.swift
+++ Sources/Hey+Commands.swift
@@ -1,17 +1,5 @@
 import ArgumentParser
 import AsyncHTTPClient
-import NIOHTTP1
-
-struct CodeArgs: ParsableArguments {
-  @Flag(name: [.long, .short], help: "Elaborate on the result and provide explicit explanation.")
-  var verbose = false
-
-  @Option(name: [.long, .short], help: "Helper files to prompt as context for the assistant.")
-  var context: [String] = []
-
-  @Argument(help: "The input prompt (request) for the acting agent.")
-  var prompt: [String]
-}
 
 @main
 struct Hey: ParsableCommand {
@@ -25,46 +13,6 @@ struct Hey: ParsableCommand {
 
   static func createHTTPClient() -> HTTPClient {
     return HTTPClient(eventLoopGroupProvider: .singleton)
-  }
-
-  struct Cmd: ParsableCommand {
-    static var configuration = CommandConfiguration(
-      commandName: "cmd",
-      abstract: "Write ad-hoc commands."
-    )
-
-    @OptionGroup var args: CodeArgs
-
-    mutating func run() throws {
-      let httpClient = Hey.createHTTPClient()
-      defer { try! httpClient.syncShutdown() }
-
-      let bodyString = "{\"key\": \"value\"}"
-      let request = try HTTPClient.Request(
-        url: "https://webhook.site/672f9dd2-070c-44a2-9886-89e971f74e2e",
-        method: .POST,
-        headers: HTTPHeaders([("User-Agent", "Hey CLI-Assistant")]),
-        body: HTTPClient.Body.data(bodyString.data(using: .utf8)!)
-      )
-
-      let responseFuture = httpClient.execute(request: request)
-      responseFuture.whenComplete { result in
-        switch result {
-        case .failure(let error):
-          print("Client seems to have failed: \(error)")
-        case .success(let response):
-          if response.status == .ok, let body = response.body {
-            print("SUCCESS -- \(String(buffer: body))")
-          } else {
-            print("Server seems to have failed iternally -- Error code: \(response.status)")
-          }
-        }
-      }
-
-      let verbose = args.verbose ? "" : "non-"
-      print("Cmd[\(verbose + "verbose")]: with prompt[\(args.prompt.joined(separator: " "))]")
-      _ = try! responseFuture.wait()
-    }
   }
 
   struct Script: ParsableCommand {
