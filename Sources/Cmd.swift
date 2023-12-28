003c31a8654af4e77f0700719c2d5c02b839c07f
Rami Kader <ramy.kader.rk@gmail.com>
2023-12-28 05:56:21 +0100

    indep subcommands

--- Sources/Cmd.swift
+++ Sources/Cmd.swift
@@ -1,17 +1,15 @@
 import ArgumentParser
 
-extension Hey {
-    struct Cmd: ParsableCommand {
-        static var configuration = CommandConfiguration(
-            commandName: "cmd",
-            abstract: "Write ad-hoc commands."
-        )
-        
-        @Argument(help: "The input prompt (request) for the acting agent.")
-        var prompt: [String]
-        
-        mutating func run() throws {
-            print(prompt)
-        }
+struct Cmd: ParsableCommand {
+    static var configuration = CommandConfiguration(
+        commandName: "cmd",
+        abstract: "Write ad-hoc commands."
+    )
+    
+    @Argument(help: "The input prompt (request) for the acting agent.")
+    var prompt: [String]
+    
+    mutating func run() throws {
+        print(prompt)
     }
 }
